48703a955d7bf1035fa052bbeff33902e8c3ea0c
webcoders <webcoders1122@gmail.com>
2024-03-07 16:30:16 +0500

    task complete feat added

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,45 @@ import { useState } from "react";
 
 function App() {
   //total task array
-  const [tasks, setTasks] = useState(["sample Task", "sample Task 2"]);
+  const [tasks, setTasks] = useState([
+    { task: "sample", complete: true },
+    { task: "sample 2", complete: false },
+  ]);
   //new task
-  const [newTask, setNewTask] = useState("");
+  const [newTask, setNewTask] = useState({ task: "", complete: false });
 
-  //function to add newTask to "task array"
+  //function to add, delete, update newTask to "task array"
   const addTask = (task) => {
-    if (task == "") return alert("Please Enter a Task");
+    if (task.task == "") return alert("Please Enter a Task");
     const newTasks = [...tasks];
     newTasks.push(task);
     setTasks(newTasks);
-    setNewTask("");
+    setNewTask({ task: "", complete: false });
   };
-
-  //funtion to delete task from "task array"
   const deleteTask = (index) => {
     const newTasks = [...tasks];
     newTasks.splice(index, 1);
     setTasks(newTasks);
   };
-
+  const updateTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1, {
+      task: newTasks[index].task,
+      complete: !newTasks[index].complete,
+    });
+    setTasks(newTasks);
+  };
   return (
     <div className='App m-2'>
       <input
         className='form-control'
         type='text'
         name='task'
-        value={newTask}
+        value={newTask.task}
         placeholder='Enter New Task Here'
-        onChange={(e) => setNewTask(e.target.value)}
+        onChange={(e) =>
+          setNewTask({ ...newTask, [e.target.name]: e.target.value })
+        }
       />
       <button
         className='btn btn-success w-100'
@@ -46,9 +56,11 @@ function App() {
         {tasks.map((task, index) => {
           return (
             <ListGroup.Item
+              variant={task.complete ? "success" : ""}
               key={index}
-              onClick={() => deleteTask(index)}>
-              {task}
+              onClick={() => updateTask(index)}
+              onDoubleClick={() => deleteTask(index)}>
+              {task.task}
             </ListGroup.Item>
           );
         })}
